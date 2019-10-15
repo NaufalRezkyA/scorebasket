3e0ae4aab66a9e4233624d7532f8fa569d7fb996
Naufal <naufalrezkyananda08@gmail.com>
2019-10-15 11:39:38 +0700

    Score Basket

--- app/src/main/java/com/naufalr/helloworld/MainActivity.java
+++ app/src/main/java/com/naufalr/helloworld/MainActivity.java
@@ -3,12 +3,22 @@ package com.naufalr.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int scoreA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tambah3PoinA(View view) {
+        scoreA =scoreA + 3;
+        TextView textviewA = findViewById(R.id.scoreA);
+        textviewA.setText(String.valueOf(scoreA));
     }
 }
